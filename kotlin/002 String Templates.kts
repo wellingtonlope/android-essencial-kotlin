c466821852f56ecc8f632e36a2b08bf553767cac
Wellington Lopes <wellingtonlopes@tce.rr.leg.br>
2019-10-24 13:16:20 -0400

    introdução kotlin

---
+++ kotlin/002 String Templates.kts
@@ -0,0 +1,5 @@
+var nome = "Ricardo"
+println("Olá $nome")
+println("$nome possui ${nome.length} caracteres")
+val msg = "Meu nome é $nome"
+println(msg)
---
+++ kotlin/003 Variáveis com var e val.kts
@@ -0,0 +1,9 @@
+var nome = "Ricardo"
+println("Olá $nome")
+nome = "Ricardo Lecheta"
+println("Olá $nome")
+
+val nomeC = "Ricardo"
+println("Olá $nomeC")
+nomeC = "Ricardo Lecheta"
+println("Olá $nomeC")
