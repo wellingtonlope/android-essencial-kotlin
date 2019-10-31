ec733ee04308450dba778f4ab13030adabd881c9
Wellington Lopes <wellington.lopes.tw@gmail.com>
2019-10-31 13:13:37 -0400

    ajustes

--- Carros/app/src/main/java/br/com/livroandroid/carros/fragments/CarrosFragment.kt
+++ Carros/app/src/main/java/br/com/livroandroid/carros/fragments/CarrosFragment.kt
@@ -6,18 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import br.com.livroandroid.carros.R
 import br.com.livroandroid.carros.adapter.CarroAdapter
 import br.com.livroandroid.carros.domain.Carro
 import br.com.livroandroid.carros.domain.CarroService
 import br.com.livroandroid.carros.domain.TipoCarro
 import br.com.livroandroid.carros.extensions.toast
+import kotlinx.android.synthetic.main.fragment_carros.*
 
 class CarrosFragment : BaseFragment() {
     private var tipo: TipoCarro = TipoCarro.classicos
     private var carros = listOf<Carro>()
-    var recyclerView: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,10 +36,9 @@ class CarrosFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Views
-        recyclerView = view.findViewById(R.id.recyclerView)
-        recyclerView?.layoutManager = LinearLayoutManager(activity)
-        recyclerView?.itemAnimator = DefaultItemAnimator()
-        recyclerView?.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.setHasFixedSize(true)
     }
 
     override fun onResume() {
@@ -52,11 +50,11 @@ class CarrosFragment : BaseFragment() {
         // Busca os carros
         this.carros = CarroService.getCarros(context, tipo)
         // Atualiza a lista
-        recyclerView?.adapter = CarroAdapter(
-            carros
-        ) { carro: Carro ->
-            toast("@Clicou no carro ${carro.nome}")
-        }
+        recyclerView.adapter = CarroAdapter(carros) { onClickCarro(it) }
+    }
+
+    fun onClickCarro(carro: Carro) {
+        toast("@Clicou no carro ${carro.nome}")
     }
 
 }
